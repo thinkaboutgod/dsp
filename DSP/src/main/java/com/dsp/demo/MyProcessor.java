2a5c5a12c8dda2f3be22245b6773c219433fb1c6
thinkaboutgod <51179602+thinkaboutgod@users.noreply.github.com>
2019-06-04 05:33:29 +0800

    提交到徳承分支

--- DSP/src/main/java/com/dsp/demo/MyProcessor.java
+++ DSP/src/main/java/com/dsp/demo/MyProcessor.java
@@ -11,13 +11,35 @@ public class MyProcessor implements PageProcessor {
 
 	@Override
 	public void process(Page page) {
+
+		page.addTargetRequests(page.getHtml().links().regex("http://www.jppt.com.cn/theory/queue-C1-1.html").all());
 		
-		page.addTargetRequests(
-				page.getHtml().links().regex("https://blog.csdn.net/"+
-						"[a‐z 0‐9 ‐]+/article/details/[0‐9]{8}").all());//将当前页面里的所有链接都添加到目标页面中
-		page.putField("title",page.getHtml().xpath("//*"+
-				"[@id=\"mainBox\"]/main/div[1]/div[1]/h1/text()").toString());
+		int i=2;
+		String content = page.getHtml().xpath("//*[@id=\"loaded\"]/div["+i+"]").get();
 		
+		String quetion = page.getHtml().xpath("//*[@id=\"q_"+i+"\"]/p/text()").get();
+		String answer = page.getHtml().xpath("//*[@id=\"answer_"+i+"\"]/@value").get();
+		String explain = page.getHtml().xpath("//*[@id=\"explain_"+i+"\"]/@data-explain").get();
+		String aop=page.getHtml().xpath("//*[@id=\"p_16_"+i+"\"]/span/text()").get();
+		String bop=page.getHtml().xpath("//*[@id=\"p_32_"+i+"\"]/span/text()").get();
+		String cop=page.getHtml().xpath("//*[@id=\"p_64_"+i+"\"]/span/text()").get();
+		String dop=page.getHtml().xpath("//*[@id=\"p_128_"+i+"\"]/span/text()").get();
+		System.out.println("内容：" + content);
+		System.out.println("题目：" + quetion);
+		System.out.println("答案：" + answer);
+		System.out.println( "解析：" + explain);
+		System.out.println( "a选项：" + aop);
+		System.out.println( "b选项：" + bop);
+		System.out.println( "c选项：" + cop);
+		System.out.println( "d选项：" + dop);
+		
+		
+		if (content != null) { // 如果有标题和内容
+//			page.putField("title", title);
+			page.putField("content", content);
+		} else {
+			page.setSkip(true);// 跳过
+		}
 	}
 
 	@Override
@@ -26,14 +48,11 @@ public class MyProcessor implements PageProcessor {
 	}
 
 	public static void main(String[] args) {
-		
-		Spider.create( new MyProcessor() )
-		.addUrl("https://blog.csdn.net/nav/ai")
-		.addPipeline(new ConsolePipeline())
-		.addPipeline(new FilePipeline("D:/data"))//以文件方式保存
-		.run();
-		
+
+		Spider.create(new MyProcessor()).addUrl("http://www.jppt.com.cn/theory/queue-C1-1.html").addPipeline(new ConsolePipeline())
+				.addPipeline(new FilePipeline("D:/data"))// 以文件方式保存
+				.run();
+
 	}
-	
-	
+
 }
